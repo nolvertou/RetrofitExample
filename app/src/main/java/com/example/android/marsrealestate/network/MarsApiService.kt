c0ef6f1945cb65b3e16d333c1c4f7427c7ec30dc
Nolverto Urias Obeso <nolvertou@gmail.com>
2023-10-08 18:22:33 -0600

    Connecting to Internet and Get JSON data from a web server using Retrofit

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,8 +17,59 @@
 
 package com.example.android.marsrealestate.network
 
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
 private const val BASE_URL = "https://mars.udacity.com/"
 
 // TODO (02) Use Retrofit Builder with ScalarsConverterFactory and BASE_URL
+// 2.1 We use Retrofit.Builder to fetch a JSON Response from our web service and return it as a string
+// 2.2 We use ScalarsConverterFactory that supports returning strings and other primitive types so we call
+//     addConverterFactory in the builder with the instance of the ScalarsCoverterFactory
+// 2.3 Then we call BASE_URL to specify the route web address of our server endpoints
+// 2.4 Finally we call build to create the retrofit object
+
+/**
+ * Add an instance of ScalarsConverterFactory and the BASE_URL we provided, then call build()
+ * to create the Retrofit object.
+ */
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
 // TODO (03) Implement the MarsApiService interface with @GET getProperties returning a String
+// Next, we'll define an interface that explains how retrofit talks to our web server using HTTP requests
+// Retrofit, will create an object that implements our interface with all of the methods that talk
+// to the server
+// 3.1 MarsApiService defines the API our retrofit service creates
+// 3.2 Get the JSON response string
+/**
+ * Create a MarsApiService interface, and define a getProperties() method to request
+ * the JSON response string.
+ * Annotate the method with @GET, specifying the endpoint for the JSON real estate response,
+ * and create the Retrofit Call object that will start the HTTP request.
+ */
+interface MarsApiService{
+    // 3.3 Specify in the annotation the path or endpoint of the JSON resonse
+    // that we want this method to use.
+    // e.g. Retrofit append to the endpoint realstate to the BASE_URL and creates the call object
+    @GET("realestate")
+    // 3.4 Call object is used to start the request and create a retofit service
+    fun getProperties():
+            Call<String>
+}
+
 // TODO (04) Create the MarsApi object using Retrofit to implement the MarsApiService
+// Since this retrofit create calls is expensive and our app only needs one retrofit service instance,
+// we'll expose the rest of the application using a public object called MarsApi
+object MarsApi{
+    // 4.1 In MarsApi object we'll add a lazily initialized retrofit object propertly named
+    // retrofitService, which gets initialized using retrofit.create method with out MarsApiService
+    // Interface
+    val retrofitService: MarsApiService by lazy{
+        retrofit.create(MarsApiService::class.java)
+    }
+}
--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -20,6 +20,11 @@ package com.example.android.marsrealestate.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsApiService
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -45,6 +50,34 @@ class OverviewViewModel : ViewModel() {
      */
     private fun getMarsRealEstateProperties() {
         // TODO (05) Call the MarsApi to enqueue the Retrofit request, implementing the callbacks
+        // 5.1 We call MarsAPI our singleton.Mars.getProperties which returns a call object
+        // 5.2 We then call enqueue on that callback to start the network request on a background thread
+        //     The enqueue method takes a retrofit callback class input that contains methods
+        //     that will be called when the request is complete.
+        //     The callback has both, success and failure methods, which are called accordingly when
+        //     retrofit is successful in fetching the JSON or an error occurs. So lets defin a Kotlin
+        //     object to implement this callback
+        // 5.3 I finish creating the object by pressing <CTRL><i> as a shortcut to implement methods
+        //      within Android Studio.
+        MarsApi.retrofitService.getProperties().enqueue(object: retrofit2.Callback<String>{
+
+            // 5.4 The _response is a string live data that determines what's shown in the TextView
+            //     Each states needs to update the _response live data
+            //     For onResponse, we have successfully gotten a response from our server.
+            //     So, we get the value using response.body()
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                _response.value = response.body()
+
+            }
+
+            // 5.5 For onFailure, we'll set response to failure plus the message from the throwable.
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                _response.value = "Failure: " + t.message
+            }
+
+        })
+
+
         _response.value = "Set the Mars API Response here __!"
     }
 }
